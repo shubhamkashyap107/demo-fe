d1004d2e2cf8ad659d0a3fd7c85830eff996cb8f
Shubham Kashyap <shubhamkashyap107@gmail.com>
2024-12-07 14:54:41 +0530

    Commit

--- src/Components/AllMentors.jsx
+++ src/Components/AllMentors.jsx
@@ -9,7 +9,7 @@ const AllMentors = () => {
     useEffect(() => {
         const getData = async () => {
 
-            const res = await fetch("https://demo-fe-km9m.onrender.com/mentors")
+            const res = await fetch("https://demo-be-x9vd.onrender.com/mentors")
             const data = await res.json()
 
             setMentorsData(data)
